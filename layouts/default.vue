882eb04358a06eac99b10dcd725116507fa937aa
yuuki-nishino <yuukibasket24center5@icloud.com>
2023-08-31 07:10:50 +0900

    deploy hydledge

---
+++ layouts/default.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app>
+    <TheHeader />
+    <v-sheet elevation="0" height="70" width="100%"></v-sheet>
+    <v-main>
+      <Nuxt />
+    </v-main>
+    <TheFooter />
+  </v-app>
+</template>
+
+<script>
+import TheHeader from '~/components/layouts/TheHeader.vue'
+import TheFooter from '~/components/layouts/TheFooter.vue'
+export default {
+  name: 'DefaultLayout',
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Old',
+          to: '/old',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <!-- <AboutSec /> -->
+    <PurposeSec />
+    <v-divider></v-divider>
+    <MissionSec />
+    <v-divider></v-divider>
+    <ContentsSec />
+    <v-divider></v-divider>
+    <NewsSec />
+  </div>
+</template>
+
+<script>
+// import AboutSec from '~/components/pages/AboutSec.vue'
+import PurposeSec from '~/components/pages/PurposeSec.vue'
+import MissionSec from '~/components/pages/MissionSec.vue'
+import ContentsSec from '~/components/pages/ContentsSec.vue'
+import NewsSec from '~/components/pages/NewsSec.vue'
+
+export default {
+  name: 'TopPage',
+  components: { PurposeSec, MissionSec, ContentsSec, NewsSec },
+  data() {
+    return {
+      // breadcrumbs: [
+      //   {
+      //     name: 'トップページ',
+      //     path: '/',
+      //   },
+      // ],
+    }
+  },
+  computed: {
+    breadcrumbs() {
+      return [
+        {
+          name: 'ホーム',
+          path: '/',
+        },
+      ]
+    },
+  },
+}
+</script>
